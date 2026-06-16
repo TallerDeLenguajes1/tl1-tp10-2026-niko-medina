831c30444d6dfac08af05b2bf10e0c525fc029db
Nicolas Medina <nikomedinaniko@gmail.com>
2026-06-16 12:20:33 -0300

    Ejercicio 1 incompleto

--- Tareas/Program.cs
+++ Tareas/Program.cs
@@ -1,2 +1,57 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Runtime.CompilerServices;
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+﻿using System.Net.WebSockets;
+using System.Text.Json;
+
+
+List<Tarea> tareas = await GetTareas();
+
+// mostrar primero las tareas pendientes
+foreach(Tarea tarea in tareas){
+    if(!tarea.Completed){
+        Console.WriteLine($"Titulo: {tarea.Title}- Estado: Pendiente");
+    }
+}
+foreach(Tarea tarea in tareas){
+    if(tarea.Completed){
+        Console.WriteLine($"Titulo: {tarea.Title}- Estado: Completada");
+    }
+}
+
+// serializar y guardar en tareas.json
+string json = JsonSerializer.Serialize(tareas);
+Console.WriteLine(json);
+
+
+static async Task<List<Tarea>> GetTareas(){
+    var url = "https://jsonplaceholder.typicode.com/todos/";
+    try
+        {
+            HttpClient client = new HttpClient();
+            HttpResponseMessage response = await client.GetAsync(url);
+            response.EnsureSuccessStatusCode();
+            string responseBody = await response.Content.ReadAsStringAsync();
+            List<Tarea> tareas = JsonSerializer.Deserialize<List<Tarea>>(responseBody);
+            return tareas;
+        }
+        catch (HttpRequestException e)
+        {
+            Console.WriteLine("Problemas de acceso a la API");
+            Console.WriteLine("Message :{0} ", e.Message);
+            return null;
+        }
+}
+
+public class Tarea{
+    [JsonPropertyName("userId")]
+    public int UserId { get; set; }
+    [JsonPropertyName("id")]
+    public int ID { get; set; }
+    [JsonPropertyName("title")]
+    public string Title { get; set; }
+    [JsonPropertyName("completed")]
+    public bool Completed { get; set; }
+}
+
+
